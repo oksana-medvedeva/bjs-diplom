a56a849019f84c7fcb5cf7bbc6843d852a202be4
Oksana Staeglazova <oksanik.med@gmail.com>
2021-08-29 17:10:47 +0300

    *task loginPage done; *task homePage done

---
+++ public/homePage.js
@@ -0,0 +1,99 @@
+// Logout
+let logoutButton = new LogoutButton();
+logoutButton.action = () => {
+	ApiConnector.logout(response => {
+		console.log(response);
+		if (response.success) {
+			location.reload();
+		}
+	})
+}
+
+// Profile
+ApiConnector.current(response => {
+	console.log(response);
+	if (response.success) {
+		ProfileWidget.showProfile(response.data);
+	}
+})
+
+// Currency rates
+let ratesBoard = new RatesBoard();
+ratesBoard.requestCurrencyRates = () => {
+	ApiConnector.getStocks(response => {
+		console.log(response)
+		if (response.success) {
+			ratesBoard.clearTable()
+			ratesBoard.fillTable(response.data)
+		}
+	})
+}
+ratesBoard.requestCurrencyRates();
+setInterval(() => ratesBoard.requestCurrencyRates(), 60000);
+
+// Money transactions
+let moneyManager = new MoneyManager();
+
+// Balance
+moneyManager.addMoneyCallback = data => {
+	ApiConnector.addMoney(data, response => {
+		console.log(response)
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+		}
+		moneyManager.setMessage(response.success, response.error);
+	})
+}
+// Currency conversion
+moneyManager.conversionMoneyCallback = data => {
+	ApiConnector.convertMoney(data, response => {
+		console.log(response)
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+		}
+		moneyManager.setMessage(response.success, response.error);
+	})
+}
+// Money transfer
+moneyManager.sendMoneyCallback = data => {
+	ApiConnector.transferMoney(data, response => {
+		console.log(response)
+		if (response.success) {
+			ProfileWidget.showProfile(response.data);
+		}
+		moneyManager.setMessage(response.success, response.error);
+	})
+}
+
+// Favourites
+let favoritesWidget = new FavoritesWidget();
+ApiConnector.getFavorites(response => {
+	console.log(response)
+	if (response.success) {
+		favoritesWidget.clearTable();
+		favoritesWidget.fillTable(response.data);
+		moneyManager.updateUsersList(response.data);
+	}
+})
+favoritesWidget.addUserCallback = data => {
+	ApiConnector.addUserToFavorites(data, response => {
+		console.log(response)
+		if (response.success) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			moneyManager.updateUsersList(response.data);
+		}
+		favoritesWidget.setMessage(response.success, response.error);
+	})
+}
+favoritesWidget.removeUserCallback = data => {
+	ApiConnector.removeUserFromFavorites(data, response => {
+		console.log(response)
+		if (response.success) {
+			favoritesWidget.clearTable();
+			favoritesWidget.fillTable(response.data);
+			moneyManager.updateUsersList(response.data);
+		}
+		favoritesWidget.setMessage(response.success, response.error);
+	})
+}
---
+++ public/loginPage.js
@@ -0,0 +1,23 @@
+"use strict";
+
+let userForm = new UserForm();
+userForm.loginFormCallback = data => {
+	ApiConnector.login(data, response => {
+		console.log(response)
+		if (response.success) {
+			location.reload();
+		} else {
+			userForm.setLoginErrorMessage(response.error);
+		}
+	})
+};
+userForm.registerFormCallback = data => {
+	ApiConnector.register(data, response => {
+		console.log(response)
+		if (response.success) {
+			location.reload();
+		} else {
+			userForm.setRegisterErrorMessage(response.error);
+		}
+	})
+};
